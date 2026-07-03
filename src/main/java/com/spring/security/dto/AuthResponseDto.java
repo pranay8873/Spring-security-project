9be374ddf591c6945099b2cf6c295acf5551f7c7
pranay8873 <pranaypuyaati@gmail.com>
2026-07-03 23:02:52 +0530

    authentication implementation

---
+++ src/main/java/com/spring/security/dto/AuthResponseDto.java
@@ -0,0 +1,12 @@
+package com.spring.security.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthResponseDto {
+    private boolean success;
+    private String message;
+    private int statuscode;
+    private String accesstoken;
+    private String refreshtoken;
+}
---
+++ src/main/java/com/spring/security/service/AuthServ.java
@@ -0,0 +1,14 @@
+package com.spring.security.service;
+
+import com.spring.security.dto.UserAuthResponseDto;
+import com.spring.security.dto.UserLoginDto;
+import com.spring.security.dto.UserResponseDto;
+import com.spring.security.dto.UserSignupDto;
+import com.spring.security.exception.UserExistException;
+import com.spring.security.exception.UserNotFoundException;
+
+public interface AuthServ {
+ UserResponseDto registerUser(UserSignupDto userSignupDto) throws UserExistException;
+ UserResponseDto loginuser(UserLoginDto userLoginDto) throws UserNotFoundException;
+
+}
